344bf64f1568a40e44eb0a3dc5c7d58c42803a36
hannah98 <furiousgeorgecode@gmail.com>
2016-11-08 09:07:20 -0600

    Added fontawesome and fixed up the titlebar

--- reddit/views.py
+++ reddit/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.template import loader
-
-# Create your views here.
 from django.http import HttpResponse
-#import html
 
 import json, collections
 from urllib import request
@@ -20,26 +17,12 @@ def subreddit(request,subreddit):
     r = praw.Reddit(user_agent=UA)
     sr = r.get_subreddit(subreddit)
     submissions = sr.get_hot(limit=25)
-    context = { 'submissions': submissions }
+    context = { 'submissions': submissions, 'subreddit': subreddit }
     #logger.error(vars(submission))
     return render(request, 'reddit/subreddit.html', context)
-        
-    #return HttpResponse("Subreddit %s: %s" % subreddit,j)
 
 def comments(request,subreddit,postid):
     r = praw.Reddit(user_agent=UA)
     submission = r.get_submission(submission_id = postid)
-    flat_comments = submission.comments
-    context = { 'submission': submission, 'comments': flat_comments, 'title': submission.title }
+    context = { 'submission': submission, 'comments': submission.comments, 'subreddit': subreddit }
     return render(request, 'reddit/comments_try.html', context)
-    
-
-def comments_flat(request,subreddit,postid):
-    r = praw.Reddit(user_agent=UA)
-    submission = r.get_submission(submission_id = postid)
-    flat_comments = praw.helpers.flatten_tree(submission.comments)
-    #logger.error(vars(flat_comments[1]))
-    #logger.error(json.dumps(submission.comments.__dict__))
-    context = { 'submission': submission, 'comments': flat_comments }
-    return render(request, 'reddit/comments.html', context)
-
