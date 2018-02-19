42f6fb2b55dadbeb06c4307fb21c26b0666adc29
hannah98 <hannah98@users.noreply.github.com>
2018-02-19 10:17:13 -0600

    Adding filters for timestamps

--- reddit/templatetags/fix_urls.py
+++ reddit/templatetags/fix_urls.py
@@ -8,3 +8,25 @@ def fixURL(string):
     line = re.sub(r"^https?://(www.)?reddit.com", "", string)
     return line
 
+@register.filter("print_timestamp")
+def print_timestamp(timestamp):
+    try:
+        #assume, that timestamp is given in seconds with decimal point
+        ts = float(timestamp)
+    except ValueError:
+        return None
+    return datetime.fromtimestamp(ts)
+
+@register.filter
+def age(value):
+    now = datetime.now()
+    try:
+        ts = float(value)
+        finalts = datetime.fromtimestamp(ts)
+        difference = now - finalts
+    except:
+        return 'some time ago'
+
+    if difference <= timedelta(minutes=1):
+        return 'just now'
+    return '%(time)s ago' % {'time': timesince(finalts).split(', ')[0]}
