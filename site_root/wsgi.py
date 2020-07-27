f47a5c3b814f214c1e9be22dab1fd0baf352a3c3
hannah98 <furiousgeorgecode@gmail.com>
2020-07-27 09:28:15 -0500

    Renamed 'redditclone' to 'site_root' because it was confusing to have a 'reddit' project and a 'redditclone' project

--- site_root/wsgi.py
+++ site_root/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for redditclone project.
+WSGI config for site_root project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redditclone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_root.settings")
 
 application = get_wsgi_application()
