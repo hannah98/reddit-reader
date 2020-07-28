f8fc1d222f551e626812fdf055d64b71b9e7e580
hannah98 <furiousgeorgecode@gmail.com>
2020-07-28 10:06:54 -0500

    Try using version script to get the version - 'latest' was not the version number I wanted

--- version.sh
+++ version.sh
@@ -5,4 +5,8 @@ VERSION_BASE="1.0"
 BUILDNUMBER=$(date "+%j%H%M%S")
 VERSION="${VERSION_BASE}.${BUILDNUMBER}"
 
+if [ -n "$DOCKER_TAG" ] && [ "$DOCKER_TAG" != "latest" ]; then
+    VERSION="$DOCKER_TAG"
+fi
+
 echo "$VERSION"
