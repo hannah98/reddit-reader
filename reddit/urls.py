768a8a73912458a402455b18f259ccb584dca3b5
hannah98 <furiousgeorgecode@gmail.com>
2020-07-28 10:53:32 -0500

    fixed a bug with trailing slashes

--- reddit/urls.py
+++ reddit/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<subreddit>[^/]+)/$', views.subreddit, name='subreddit'),
+    url(r'^(?P<subreddit>[^/]+)[/]?$', views.subreddit, name='subreddit'),
     url(r'^(?P<subreddit>[^/]+)/comments/(?P<postid>[^/]+)', views.comments, name='comments'),
 ]
